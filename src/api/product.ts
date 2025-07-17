edef43c98fab9b328908505eb57894983ce0d02e
SonJH7 <sjunh02@naver.com>
2025-07-18 04:55:05 +0900

    refactor(apiproduct): 타입 명확한 형태로 표현 수정

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,10 +1,10 @@
-﻿export async function fetchProductRanking(targetType: string = 'ALL', rankType: string = 'MANY_WISH') {
-  const params = new URLSearchParams({ targetType, rankType });
+﻿import type { Product } from '../type'
+export async function fetchProductRanking(targetType: string = 'ALL', rankType: string = 'MANY_WISH') {  const params = new URLSearchParams({ targetType, rankType });
   const res = await fetch(`/api/products/ranking?${params.toString()}`);
   const json = await res.json();
 
   if (!res.ok || !Array.isArray(json.data)) {
     throw new Error('Invalid response from /api/products/ranking');
   }
-  return json.data as import('../type').Product[];
+  return json.data as Product[];
 }
